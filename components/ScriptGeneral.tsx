67a0feb3caa4c91730a31592bbacf4deccc7ef36
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-01-04 11:27:45 +0300

    add
    add metrick and page speed

--- components/ScriptGeneral.tsx
+++ components/ScriptGeneral.tsx
@@ -5,49 +5,35 @@ export const ScriptGeneral = () => {
   return (
     <>
       {/* google start */}
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JBFF7RBJKL" />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-KY314T9EDJ"
+      />
       <Script id="google-analytics">
         {`
             window.dataLayer = window.dataLayer || [];
 
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){
+              dataLayer.push(arguments);
+            } 
 
             gtag('js', new Date());
-
-            gtag('config', 'G-JBFF7RBJKL');
+            gtag('config', 'G-KY314T9EDJ');
         `}
       </Script>
       {/* google end */}
 
-      {/* yandex start */}
-      <Script id="yandex-metric">
+      {/* google start */}
+      <Script id="google-metric">
         {`
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
-        
-        ym(95788534, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-        });
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MFT2XZNC');
       `}
       </Script>
-
-      <noscript>
-        <div>
-          <Image
-            src="https://mc.yandex.ru/watch/95788534"
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
-      </noscript>
-
-      {/* yandex end */}
+      {/* google end */}
     </>
   );
 };
--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -4,19 +4,23 @@ import { ReactNode } from "react";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header/Header";
 import { AlertSuccess } from "@/components/Alert/AlertSuccess";
+import { ScriptGeneral } from "@/components/ScriptGeneral";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 interface IMainLayout {
   children: ReactNode;
 }
 
-export function MainLayout ({ children }: IMainLayout) {
+export function MainLayout({ children }: IMainLayout) {
   return (
     <div className="min-h-screen max-w-full">
       <NextTopLoader />
       <AlertSuccess />
+      <ScriptGeneral />
+      <SpeedInsights />
       <Header />
       <main>{children}</main>
       <Footer />
     </div>
   );
-};
+}
