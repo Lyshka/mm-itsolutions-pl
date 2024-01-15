a19b50dfa5a84d35e2f2a16c5a04fc313a256831
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-01-15 12:10:54 +0300

    add
    add scripti-google

--- components/ScriptGeneral.tsx
+++ components/ScriptGeneral.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Script from "next/script";
 
 export const ScriptGeneral = () => {
@@ -31,6 +30,18 @@ export const ScriptGeneral = () => {
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-MFT2XZNC');
+      `}
+      </Script>
+      {/* google end */}
+
+      {/* google start */}
+      <Script id="google-analytics-2">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TTL5SDPB');
       `}
       </Script>
       {/* google end */}
